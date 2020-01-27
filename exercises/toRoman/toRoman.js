023e248af3992b06292866bbedd738346d07315f
Patrick Sullivan <pasullivan@davidson.edu>
2020-01-26 22:59:36 -0800

    toRoman unfinished

--- exercises/toRoman/toRoman.js
+++ exercises/toRoman/toRoman.js
@@ -15,14 +15,47 @@
  * @returns {string} The input integer's Roman numeral representation
  */
 function toRoman(num) {
-  // This is your job. :)
+  let ones = ["I","IV","V","VI","VII","VIII","IX","X"];
+  let tens = ["X","L","C"];
+  let hundereds = ["D","M"];
+  let n = num.toString();
+  let ROM = [];
+  ROM = n.split("");
+  console.log(ROM);
+
+  console.log(ROM[ROM.length-1]);
+
+  
+  for(i = 0; i <= n; i++)
+  { 
+
+    while(ROM[ROM.length-1] === '1' || ROM[ROM.length-1] === '2' || ROM[ROM.length-1] === '3')
+      {ROMAN_NUM += ones[0]}
+
+    if(ROM[ROM.length-1] === '4')
+      {ROMAN_NUM += ones[1]}
+    if(ROM[ROM.length-1] === '5')
+      {ROMAN_NUM += ones[2]}
+    if(ROM[ROM.length-1] === '6')
+      {ROMAN_NUM += ones[3]}
+    if(ROM[ROM.length-1] === '7')
+      {ROMAN_NUM += ones[4]}
+    if(ROM[ROM.length-1] === '8')
+      {ROMAN_NUM += ones[5]}
+    if(ROM[ROM.length-1] === '9')
+      {ROMAN_NUM += ones[6]}
+
+    if(n.charAt(ROM[ROM.length-1]) === '0' && ROM[ROM.length-2] != undefined)
+      {ROMAN_NUM += ones[7]}
+  }
+  
+  return ROM;
 }
 
 if (require.main === module) {
   console.log('Running sanity checks for toRoman:');
-
-  // Add your own sanity checks here.
-  // How else will you be sure your code does what you think it does?
+  console.log(toRoman(420));
+  //console.log(toRoman(4) === "IV");
 }
 
 module.exports = toRoman;
